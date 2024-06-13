b7375fc9e38239e4a8c2e3a4ca1081e3e85856c8
Davi Fernandes <davifncosta@gmail.com>
2024-06-13 11:46:35 -0300

    fix: problems login with google and github (#8)

--- lib/auth.ts
+++ lib/auth.ts
@@ -19,10 +19,12 @@ export const authOptions: NextAuthOptions = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       name: 'Credentials',
